b1cd51a5a0142d4031ee7e07aa5db1facc9d18c1
Mark van den Broek <mvdnbrk@protonmail.com>
2025-01-04 12:31:01 +0100

    remove active state

--- resources/views/components/pulverizers.blade.php
+++ resources/views/components/pulverizers.blade.php
@@ -13,12 +13,12 @@
                 <a href="{{ route('pulverizer', ['type' => $key]) }}">
                 @svg('pulverizers.'.$key, [
                     'class' => 'w-96 md:w-24 border rounded-md'
-                        . ($key == 'white' ? ' animate-shake' : ' hover:animate-shake'),
+                        . ($key == '---' ? ' animate-shake' : ' hover:animate-shake'),
                     'title' => Str::of($key)->ucfirst()->append(' ')->append('Pizza Pet Pulverizer')->toString(),
                 ])
                 </a>
                 <span class="mt-1 text-xs font-game font-semibold text-orange-900 text-center">
-                    {{ ($key == 'white' ? 'active' : 'inactive') }}
+                    {{ ($key == '---' ? 'active' : 'inactive') }}
                 </span>
             </div>
         @endforeach
