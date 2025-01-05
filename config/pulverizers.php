44a19d4f34c617794dc380cb52b76177fe231073
Mark van den Broek <mvdnbrk@protonmail.com>
2025-01-05 17:32:43 +0100

    Update pulverizers.php

--- config/pulverizers.php
+++ config/pulverizers.php
@@ -2,16 +2,58 @@
 
 return [
 
-    'yellow' => '14580fd5072a0e96d8e0e6b1526319fde391d788b80a17eee23bf91ee1f961b2i5',
+    'yellow' => [
+        'inscription_id' => '14580fd5072a0e96d8e0e6b1526319fde391d788b80a17eee23bf91ee1f961b2i5',
+        'owner' => 'bc1puq4f6z9430umjdf3y54zcl9sc2em7grf9asd7deks9d7ycjs5umqt9rfzp',
+        'buyer_twitter_handle' => 'PipeAliens',
+        'hex_color' => '#FFD400',
+        'recharge' => 6,
+        'is_recharging' => false,
+        'status' => 'inactive',
+    ],
 
-    'white' => '14580fd5072a0e96d8e0e6b1526319fde391d788b80a17eee23bf91ee1f961b2i4',
+    'white' => [
+        'inscription_id' => '14580fd5072a0e96d8e0e6b1526319fde391d788b80a17eee23bf91ee1f961b2i4',
+        'owner' => 'bc1pdyujulg6k7v98lgzznrajef49mpnhkz4hxqkevhq6j0mqdall6kqmg3rxl',
+        'buyer_twitter_handle' => 'dogofbitcoin',
+        'hex_color' => '#EDEDED',
+        'recharge' => 5,
+        'is_recharging' => true,
+        'status' => 'inactive',
+    ],
 
-    'black' => '14580fd5072a0e96d8e0e6b1526319fde391d788b80a17eee23bf91ee1f961b2i0',
+    'black' => [
+        'inscription_id' => '14580fd5072a0e96d8e0e6b1526319fde391d788b80a17eee23bf91ee1f961b2i0',
+        'owner' => 'bc1p0sjaj6m0uha2rlh723h9rvp9r49lgjkqge5c67q2m4cfrh4x6upsyfp5zz',
+        'buyer_twitter_handle' => 'GrimReaper52210',
+        'hex_color' => '#464B4B',
+        'recharge' => 6,
+        'is_recharging' => true,
+        'status' => 'inactive',
+    ],
 
-    'red' => '14580fd5072a0e96d8e0e6b1526319fde391d788b80a17eee23bf91ee1f961b2i3',
+    'red' => [
+        'inscription_id' => '14580fd5072a0e96d8e0e6b1526319fde391d788b80a17eee23bf91ee1f961b2i3',
+        'hex_color' => '#FF002C',
+        'recharge' => 7,
+        'is_recharging' => false,
+        'status' => 'inactive',
+    ],
 
-    'green' => '14580fd5072a0e96d8e0e6b1526319fde391d788b80a17eee23bf91ee1f961b2i2',
+    'green' => [
+        'inscription_id' => '14580fd5072a0e96d8e0e6b1526319fde391d788b80a17eee23bf91ee1f961b2i2',
+        'hex_color' => '#00A900',
+        'recharge' => 7,
+        'is_recharging' => false,
+        'status' => 'inactive',
+    ],
 
-    'blue' => '14580fd5072a0e96d8e0e6b1526319fde391d788b80a17eee23bf91ee1f961b2i1',
+    'blue' => [
+        'inscription_id' => '14580fd5072a0e96d8e0e6b1526319fde391d788b80a17eee23bf91ee1f961b2i1',
+        'hex_color' => '#6089FF',
+        'recharge' => 7,
+        'is_recharging' => false,
+        'status' => 'inactive',
+    ],
 
 ];
