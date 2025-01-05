388c36ba5c2b0ba760e8eae902fadea37f6a208c
Mark van den Broek <mvdnbrk@protonmail.com>
2025-01-05 18:34:30 +0100

    code style

--- app/Providers/HorizonServiceProvider.php
+++ app/Providers/HorizonServiceProvider.php
@@ -28,8 +28,8 @@ class HorizonServiceProvider extends HorizonApplicationServiceProvider
     protected function gate(): void
     {
         Gate::define('viewHorizon', function ($user = null) {
-            if(request()->bearerToken() && request()->bearerToken() === config('services.horizon.token')){
-               return true;
+            if (request()->bearerToken() && request()->bearerToken() === config('services.horizon.token')) {
+                return true;
             }
 
             //
