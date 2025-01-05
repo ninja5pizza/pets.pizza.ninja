bd5c575fafb402b6070f6f14b17c91462fa47d1c
Mark van den Broek <mvdnbrk@protonmail.com>
2025-01-05 18:36:01 +0100

    add `function_exists` check

--- app/helpers.php
+++ app/helpers.php
@@ -1,10 +1,12 @@
 <?php
 
-function cdn_asset(string $path): string
-{
-    $cdnUrl = config('app.cdn_url');
+if (! function_exists('cdn_asset')) {
+    function cdn_asset(string $path): string
+    {
+        $cdnUrl = config('app.cdn_url');
 
-    $path = '/'.ltrim($path, '/');
+        $path = '/'.ltrim($path, '/');
 
-    return $cdnUrl.$path;
+        return $cdnUrl.$path;
+    }
 }
