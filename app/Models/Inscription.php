56f9c5b18c9c8868115e7a0e68e463487d0fd39e
Mark van den Broek <mvdnbrk@protonmail.com>
2025-02-05 17:41:26 +0100

    Add `fillable`

--- app/Models/Inscription.php
+++ app/Models/Inscription.php
@@ -7,4 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 class Inscription extends Model
 {
     const UPDATED_AT = null;
+
+    protected $fillable = [
+        'inscription_id',
+        'parent_id',
+        'name',
+        'created_at_block',
+    ];
 }
